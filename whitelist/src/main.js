521dd68102c764452f60dde6b4c340e65e75d092
Memory <admin@mcpeach.cc>
2024-12-26 20:34:47 +0800

    前端新增查询白名单页面，美化等

--- whitelist/src/main.js
+++ whitelist/src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as Icons from '@element-plus/icons-vue';
+import router from './router';
 
 const app = createApp(App);
 
 // 注册Element Plus
 app.use(ElementPlus);
+
+// 注册路由
+app.use(router);
 
 // 注册Element Plus图标
 for (const [key, component] of Object.entries(Icons)) {
