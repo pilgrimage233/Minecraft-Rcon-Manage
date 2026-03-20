794af6c83026847567cbb560fe70cde85bcd61c6
pilgrimage233 <pilgrimage233@icloud.com>
2026-03-20 10:57:42 +0800

    feat: mark threadPoolTaskExecutor bean as primary for improved async task handling

--- endless-framework/src/main/java/cc/endmc/framework/config/ThreadPoolConfig.java
+++ endless-framework/src/main/java/cc/endmc/framework/config/ThreadPoolConfig.java
@@ -4,6 +4,7 @@ import cc.endmc.common.utils.Threads;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -36,6 +37,7 @@ public class ThreadPoolConfig implements AsyncConfigurer {
     // 空闲线程存活时间（秒）
     private final int keepAliveSeconds = 300;
 
+    @Primary
     @Bean(name = "threadPoolTaskExecutor")
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
